7056fb226db65a32c926919d2959561e5f21eb6a
Catalin Ristache <Catalin_Ristache@waters.com>
2022-11-25 17:26:47 +0200

    Corrections for settings, file copy and structure creation

--- Program.cs
+++ Program.cs
@@ -12,28 +12,38 @@ namespace _LNG_Collector
 {
     internal class LNG_Situatii_Zilnice
     {
-        string settings_file = "settings.json";
+        public string settings_file = "settings.json";
         
         static void Main(string[] args)
         {
-            string data_curenta = DateTime.Now.ToString("dd/MM/yyyy");
+            //Pentru real-time environment
+            //string data_curenta = DateTime.Now.ToString("dd-MM-yyyy");
+            //string folder_curent = Directory.GetCurrentDirectory();
+
+
+            //Pentru testare
+            string data_curenta = "01-12-2022";
+            string folder_curent = @"C:\GitHub\LNG_Collector\";
 
             Console.WriteLine("Initiere Generare Situatii Zilnice LNG: " + data_curenta);
             Console.WriteLine("...");
             
             //citeste fisierele de intrare
             Console.WriteLine("Verifica existenta date de intrare...");
-            CitesteSiVerificaFisierele();
+            CitesteSiVerificaFisierele(folder_curent);
 
             //Creaza structura pentru data curenta
-            Directory.CreateDirectory(data_curenta);
-            Directory.CreateDirectory(data_curenta+@"\input");
-            Directory.CreateDirectory(data_curenta+@"\output");
+            Directory.CreateDirectory(folder_curent+@"\"+data_curenta);
+            Directory.CreateDirectory(folder_curent + @"\" + data_curenta +@"\input");
+            Directory.CreateDirectory(folder_curent + @"\" + data_curenta +@"\output");
 
             //copiaza fisierele de intrare in directorul zilnic de input
-            CopiazaFisiere(@"\transfer_input\", data_curenta + @"\input\");
+            CopiazaFisiere(folder_curent + @"\transfer_input\", folder_curent + @"\" + data_curenta + @"\input\");
             //copiaza fisierele template pentru a fi umplute cu dare in diectorul zilnic de output
-            CopiazaFisiere(@"\templates\", data_curenta + @"\output\");
+            CopiazaFisiere(folder_curent + @"\templates\", folder_curent + @"\" + data_curenta + @"\output\");
+
+            //
+
         }
 
         private static void CopiazaFisiere(string inputPath, string dailyInput)
@@ -41,30 +51,35 @@ namespace _LNG_Collector
             foreach (var newPath in Directory.GetFiles(inputPath, "*.*", SearchOption.AllDirectories))
             {
                 File.Copy(newPath, newPath.Replace(inputPath, dailyInput));
+                Console.WriteLine(newPath+": Fisier copiat in " + dailyInput);
             }
         }
 
-        private static void CitesteSiVerificaFisierele()
+        private static void CitesteSiVerificaFisierele(string folder_curent)
         {
             //citeste din setari ce fisiere trebuie sa existe
-            Setari setari = JsonConvert.DeserializeObject<Setari>(File.ReadAllText("settings.json"));
-            if (setari == null) {
+            Setari setari = new Setari();
+            try
+            {
+                setari = JsonConvert.DeserializeObject<Setari>(File.ReadAllText(folder_curent + @"\settings.json"));
+            }catch (Exception ex)
+            {
                 Console.WriteLine("Fisier de setari eronat");
                 return;
             }
-
+            
             //
             foreach (string filename in setari.InputFiles)
             {
                 //verify all files in the list
                 bool allOK = true;
-                if (File.Exists(@"\transfer_input\"+filename))
+                if (File.Exists(folder_curent + @"\transfer_input\" + filename))
                 {
-                    Console.WriteLine(@"\transfer_input\"+ filename + "-> exista. OK");
+                    Console.WriteLine(folder_curent + @"\transfer_input\" + filename + " -> exista. OK");
                 }
                 else { 
                     allOK = false;
-                    Console.WriteLine(@"\transfer_input\" + filename + " -> NU EXISTA!. NOK");
+                    Console.WriteLine(folder_curent + @"\transfer_input\" + filename + " -> NU EXISTA!. NOK");
                 }
                 if (!allOK) {
                     Console.WriteLine("Nu toate fisierele de input sunt prezente");
--- Utils/DistributionList.cs
+++ Utils/DistributionList.cs
@@ -4,6 +4,9 @@ namespace _LNG_Collector.Utils
 {
     public class DistributionList
     {
+        [JsonProperty("nume")]
+        public string Name { get; set; }
+
         [JsonProperty("email")]
         public string[] Email { get; set; }
 

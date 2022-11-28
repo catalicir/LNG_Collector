cbccdaa79f2cd55ef207c5df51c0fdf1dd0f8ace
Catalin Ristache <Catalin_Ristache@waters.com>
2022-11-28 18:15:40 +0200

    more functionality added to read the content of input and write to output

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ using System.Text.Json;
 using Newtonsoft.Json;
 using System.Net;
 using System.IO;
-using FastExcel;
 using System.Threading.Tasks;
 using _LNG_Collector.Utils;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Data;
+using System.Linq;
+using NPOI.HSSF.UserModel;
 
 namespace _LNG_Collector
 {
@@ -38,13 +42,147 @@ namespace _LNG_Collector
             Directory.CreateDirectory(folder_curent + @"\" + data_curenta +@"\output");
 
             //copiaza fisierele de intrare in directorul zilnic de input
-            CopiazaFisiere(folder_curent + @"\transfer_input\", folder_curent + @"\" + data_curenta + @"\input\");
+            
             //copiaza fisierele template pentru a fi umplute cu dare in diectorul zilnic de output
             CopiazaFisiere(folder_curent + @"\templates\", folder_curent + @"\" + data_curenta + @"\output\");
+            CopiazaFisiere(folder_curent + @"\transfer_input\", folder_curent + @"\" + data_curenta + @"\input\");
+            //redenumeste fisiere din output - trebuie?
 
-            //
+
+            //copiaza continut din input in output
+            //CopiazaContinut(@"\" + data_curenta + @"\input\", @"\" + data_curenta + @"\output\", "template 1", "A2", "C2");
+            CopiazaContinutTest(folder_curent + @"\" + data_curenta + @"\input\input 11.xlsx", folder_curent + @"\" + data_curenta + @"\output\template 01.xlsx");
+            //order information on output sheets
+
+
+            //refresh pivot tables
+            
+            
 
         }
+
+        private static void CopiazaContinutTest(string inputFilePath, string outputFilePath)
+        {
+            DataTable dtTable = new DataTable();
+            var fs = new FileStream(inputFilePath, FileMode.Open);
+            try
+            {
+                //copiaza din input 11.xls in template 01.xls:
+                //  sheet "Input 1", cols A, B de la row 2 incolo in "template 1", cols A, B de la row 2
+                //  sheet "Input 1", cols C de la row 2 incolo in ""template 1", cols C de la row 2 incolo
+
+                //creaza file streams penreu cele 2 fisiere
+                
+                List<string> rowList = new List<string>();
+                
+                using (fs)
+                {
+                    dtTable = GetDataFromFileWorksheet(dtTable, rowList, fs, 0);
+                }
+                //pentru verificare
+                string whatIveGotSoFar = JsonConvert.SerializeObject(dtTable);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Copierea continutului a esuat: ");
+                Console.WriteLine(ex.ToString());
+            }
+
+            try
+            {
+                //deschide fisierul destinatie pentru scriere
+                WriteDataToFileWorksheet(dtTable, outputFilePath, 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Scrierea continutului a esuat: ");
+                Console.WriteLine(ex.ToString());
+            }
+            finally { fs.Close(); }
+        }
+
+        private static void WriteDataToFileWorksheet(DataTable table, string outputFilePath, int worksheetno)
+        {
+            //open destination file to write
+            using (var fs = new FileStream(outputFilePath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                fs.Position = 0;
+                XSSFWorkbook workbook = new XSSFWorkbook(fs);
+                //workbook.Write(fs, true);
+                var excelSheet = workbook.GetSheetAt(worksheetno);
+
+                //List<DataColumn> columns = new List<DataColumn>();
+                IRow row = excelSheet.GetRow(1);
+                int columnIndex = 0;
+
+                // you can create also column header - comment for now
+                /*foreach (System.Data.DataColumn column in table.Columns)
+                {
+                    columns.Add(column.ColumnName);
+                    row.CreateCell(columnIndex).SetCellValue(column.ColumnName);
+                    columnIndex++;
+                }*/
+
+                int rowIndex = 1;
+                foreach (DataRow dsrow in table.Rows)
+                {
+                    //row = excelSheet.CreateRow(rowIndex);
+                    int cellIndex = 0;
+                    foreach (DataColumn col in table.Columns)
+                    {
+                        if (row == null) { 
+                            row = excelSheet.CreateRow(rowIndex++);
+                        }
+                        row.CreateCell(cellIndex).SetCellValue(dsrow[col].ToString());
+                        cellIndex++;
+                    }
+
+                    rowIndex++;
+                }
+
+                workbook.Write(fs, false);
+                fs.Close(); 
+            }
+        }
+
+        private static DataTable GetDataFromFileWorksheet(DataTable dtTable, List<string> rowList, FileStream fs, int worksheetno)
+        {
+            IWorkbook workbook = new XSSFWorkbook(fs);
+            var sheet = workbook.GetSheetAt(worksheetno);
+            IRow headerRow = sheet.GetRow(0);
+            int cellCount = headerRow.LastCellNum;
+            for (int j = 0; j < cellCount; j++)
+            {
+                ICell cell = headerRow.GetCell(j);
+                if (cell == null || string.IsNullOrWhiteSpace(cell.ToString())) continue;
+                {
+                    dtTable.Columns.Add(cell.ToString());
+                }
+            }
+            for (int i = (sheet.FirstRowNum); i <= sheet.LastRowNum; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                if (row == null) continue;
+                if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                for (int j = row.FirstCellNum; j < cellCount; j++)
+                {
+                    if (row.GetCell(j) != null)
+                    {
+                        if (!string.IsNullOrEmpty(row.GetCell(j).ToString()) && !string.IsNullOrWhiteSpace(row.GetCell(j).ToString()))
+                        {
+                            rowList.Add(row.GetCell(j).ToString());
+                        }
+                    }
+                }
+                if (rowList.Count > 0)
+                    dtTable.Rows.Add(rowList.ToArray());
+                rowList.Clear();
+            }
+            fs.Close();
+
+            return dtTable;
+        }
+
 
         private static void CopiazaFisiere(string inputPath, string dailyInput)
         {
@@ -64,7 +202,7 @@ namespace _LNG_Collector
                 setari = JsonConvert.DeserializeObject<Setari>(File.ReadAllText(folder_curent + @"\settings.json"));
             }catch (Exception ex)
             {
-                Console.WriteLine("Fisier de setari eronat");
+                Console.WriteLine("Fisier de setari eronat" + ex);
                 return;
             }
             
---
+++ Utils/InputFile1StructureSheet1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _LNG_Collector.Utils
+{
+    internal class InputFile1StructureSheet1
+    {
+        public string StringColumnA { get; set; }
+        public DateTime DateColumnB { get; set; }
+        public double DoubleColumnC { get; set; }
+    }
+}
